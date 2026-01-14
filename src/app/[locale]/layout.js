62ddd8a4a9b2fb3bdcb4eb1265c3359f6a13425d
webteamgtc <144129216+webteamgtc@users.noreply.github.com>
2026-01-14 14:28:40 +0400

    updayed sdas

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -7,9 +7,7 @@ import Script from "next/script";
 
 export default async function LocaleLayout({ children, params }) {
   const { locale } = await params;
-  if (!hasLocale(routing.locales, locale)) {
-    notFound();
-  }
+  if (!hasLocale(routing.locales, locale)) notFound();
 
   const isArabicOrKurdish = locale === "ar" || locale === "ku";
 
@@ -23,7 +21,6 @@ export default async function LocaleLayout({ children, params }) {
       }}
     >
       <head>
-        {/* Fonts */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link
@@ -40,35 +37,6 @@ export default async function LocaleLayout({ children, params }) {
           content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
         />
 
-        {/* TikTok Pixel */}
-        <Script id="tiktok-pixel" strategy="afterInteractive">
-          {`
-!function (w, d, t) {
-  w.TiktokAnalyticsObject=t;
-  var ttq=w[t]=w[t]||[];
-  ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"];
-  ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}; 
-  for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);
-  ttq.instance=function(t){for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e};
-  ttq.load=function(e,n){
-    var r="https://analytics.tiktok.com/i18n/pixel/events.js",o=n&&n.partner;
-    ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=r,
-    ttq._t=ttq._t||{},ttq._t[e]=+new Date,
-    ttq._o=ttq._o||{},ttq._o[e]=n||{};
-    n=document.createElement("script");
-    n.type="text/javascript";n.async=!0;
-    n.src=r+"?sdkid="+e+"&lib="+t;
-    e=document.getElementsByTagName("script")[0];
-    e.parentNode.insertBefore(n,e)
-  };
-
-  ttq.load('D5J64UJC77UAODHQ4B4G');
-  ttq.page();
-}(window, document, 'ttq');
-          `}
-        </Script>
-
-        {/* Font override styles */}
         <style>{`
           html, body, *, *::before, *::after {
             font-family: "PingFang SC", "PingFang TC", "Hiragino Sans GB", "Microsoft YaHei", "微软雅黑", "STHeiti", "WenQuanYi Micro Hei", '${
@@ -79,6 +47,20 @@ export default async function LocaleLayout({ children, params }) {
       </head>
 
       <body>
+        {/* ✅ TikTok Pixel */}
+        <Script id="tiktok-pixel" strategy="afterInteractive">
+          {`
+!function (w, d, t) {
+  w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(
+var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var r="https://analytics.tiktok.com/i18n/pixel/events.js",o=n&&n.partner;ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=r,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};n=document.createElement("script")
+;n.type="text/javascript",n.async=!0,n.src=r+"?sdkid="+e+"&lib="+t;e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(n,e)};
+
+  ttq.load('D5JMG9RC77U666PPMF40');
+  ttq.page();
+}(window, document, 'ttq');
+          `}
+        </Script>
+
         <NextIntlClientProvider>{children}</NextIntlClientProvider>
         <ToastContainer autoClose={3000} />
       </body>
